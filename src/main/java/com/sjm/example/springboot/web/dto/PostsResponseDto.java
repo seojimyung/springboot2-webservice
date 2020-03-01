fc3e2ef65df18db44b4120c3f0dfb5b80bb1c0e7
sjm <galy1@hanmail.net>
2020-03-01 22:56:36 +0900

    4장까지 완료

---
+++ src/main/java/com/sjm/example/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.sjm.example.springboot.web.dto;
+
+import com.sjm.example.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
